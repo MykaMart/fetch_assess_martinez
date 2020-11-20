c0e0845b7a4de2c65f5cd71b90be8cf88a35b954
MykaMart <myka@mykaintech.com>
2020-11-20 11:39:23 -0600

    Process/Sort/Filter Items

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let items;
+let processedItems;
 
 const processItems = async () => {
 
@@ -10,7 +10,47 @@ const processItems = async () => {
 
 	const items = await fetch(corsProxy + fetchAPI)
 						.then(res => res.json())
+
+	return items
 }
 
+const sortFilterItems = (items) => {
+	// Filter Unnamed Items
+	console.log(items)
+	let filteredItems = items.filter((item) => {
+		let named = true
+		if (item.name == "" || item.name == null){
+			named = false
+		}
+		return named
+	})
 
+	// Sort Items
 
+	let sortedItems = filteredItems.sort((item1, item2) => {
+
+		let itemNumber1 = parseInt(item1.name.substring(5))
+		let itemNumber2 = parseInt(item2.name.substring(5))
+
+		if (item1.listId > item2.listId){
+			return 1
+		} else if (item1.listId < item2.listId) {
+			return -1
+		}
+
+		if (itemNumber1 > itemNumber2){
+			return 1
+		} else if (itemNumber1 < itemNumber2) {
+			return -1
+		}
+	})
+
+	return sortedItems
+
+}
+
+processItems()
+	.then(response => sortFilterItems(response))
+	.then(processed => {processedItems = processed})
+
+console.log(processedItems)
